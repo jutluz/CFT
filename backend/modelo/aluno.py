f4437a5ba46a70e130e92ac597180c8b0c334517
Júlia Luz <94867831+jutluz@users.noreply.github.com>
2022-10-03 13:40:12 -0300

    criando a classe aluno com herança de pessoa

---
+++ backend/modelo/aluno.py
@@ -0,0 +1,14 @@
+from geral.config import *
+from modelo.pessoa import Pessoa
+
+class Aluno(Pessoa):
+    #criação da tabela
+    __tablename__ = "aluno"
+
+    #chave estrangeira
+    id = db.Column(db.Integer, db.ForeignKey("pessoa.id"), primary_key=True)
+
+    #type na classe mãe Pessoa
+    __mapper_args__ = {
+        "polymorphic_identity": "aluno",
+    }
