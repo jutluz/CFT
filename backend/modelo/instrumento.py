4b567b9a02af1eb4993d8875b50ea2ea51279507
Júlia Luz <94867831+jutluz@users.noreply.github.com>
2022-12-12 23:06:18 -0300

    Ajeitando comentários

--- backend/modelo/instrumento.py
+++ backend/modelo/instrumento.py
@@ -2,20 +2,21 @@ from geral.config import *
 from modelo.turma import Turma
 
 class Instrumento(db.Model):
-    #criação da tabela
+    # criação da tabela
     __tablename__ = "instrumento"
 
-    #colunas com os atributos pra tabela no SQLAlchemy
+    # colunas com os atributos pra tabela no SQLAlchemy
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(254), nullable=False)
 
     turma_id = db.Column(db.Integer, db.ForeignKey(Turma.id))
-    #na lista reversa em Turma já é criado o atributo "turma" em Instrumento
+    # na lista reversa em Turma já é criado o atributo "turma" em Instrumento
 
-    #função pra transformar os atributos em json
+    # função pra transformar os atributos em json
     def json(self):
         return {
             "id":self.id,
             "nome":self.nome,
             "turma_id":self.turma_id
-        } 
+            # "musica_id":self.musica_id
+        }
