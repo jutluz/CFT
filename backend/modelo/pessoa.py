3145bdea52b214f8f00e526aa36960d694ac5909
Júlia Luz <94867831+jutluz@users.noreply.github.com>
2022-12-12 23:07:49 -0300

    Ajeitando os comentários

--- backend/modelo/pessoa.py
+++ backend/modelo/pessoa.py
@@ -2,27 +2,27 @@ from geral.config import *
 from modelo.turma import Turma
 
 class Pessoa(db.Model):
-    #criação da tabela
+    # criação da tabela
     __tablename__ = "pessoa"
 
-    #colunas com os atributos pra tabela no SQLAlchemy    
+    # colunas com os atributos pra tabela no SQLAlchemy    
     id = db.Column(db.Integer, primary_key = True)
     nome = db.Column(db.String(254), nullable = False)
     email = db.Column(db.String(254), nullable = False, unique = True)
-    senha = db.Column(db.String(254), nullable = False)
+    senha = db.Column(db.Text)
     type = db.Column(db.String(50)) #atributo discriminador
 
-    #identidade polimórfica da classe
+    # identidade polimórfica da classe
     __mapper_args__ = {
         "polymorphic_identity": "pessoa",
         "polymorphic_on": type,
     }
 
-    #associação com a classe Turma
+    # associação com a classe Turma
     turma_id = db.Column(db.Integer, db.ForeignKey('turma.id'), nullable = False)
     turma = db.relationship("Turma", back_populates="pessoa")
 
-    #função pra transformar os atributos em json
+    # função pra transformar os atributos em json
     def json(self):
         return {
             "id": self.id,
