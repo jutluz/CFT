3fe494274ff55b44f58d915c00cd3c3ab146d0a0
Júlia Luz <94867831+jutluz@users.noreply.github.com>
2022-12-12 23:12:21 -0300

    Adicionando a importação de classes

--- backend/criar_tabelas.py
+++ backend/criar_tabelas.py
@@ -1,14 +1,9 @@
-from geral.config import *
-from modelo.professor import *
-from modelo.aluno import *
-from modelo.pessoa import *
-from modelo.turma import *
-from modelo.instrumento import *
-from modelo.agenda import *
+from importacoes import *
 
+# cada vez que rodar esse arquivo as tabelas serão restauradas, então para preencher as tabelas vá para "teste_classes"
 if __name__ == "__main__":
-    #db.drop_all()
-    #print("Tabelas excluídas")
+    db.drop_all()
+    print("Tabelas excluídas")
     
     db.create_all()
     print("Tabelas criadas")
