e040e8d53be20575b59c95ac6ead4b0d5bd95c75
Júlia Luz <94867831+jutluz@users.noreply.github.com>
2022-12-12 23:08:42 -0300

    Adicionando a função json nos atributos

--- backend/modelo/professor.py
+++ backend/modelo/professor.py
@@ -2,21 +2,26 @@ from geral.config import *
 from modelo.pessoa import Pessoa
 
 class Professor(Pessoa):
-    #criação da tabela
+    # criação da tabela
     __tablename__ = "professor"
 
-    #chave estrangeira
+    # chave estrangeira
     id = db.Column(db.Integer, db.ForeignKey("pessoa.id"), primary_key=True)
     salario = db.Column(db.Float, nullable = False)
 
-    #type na classe mãe -Pessoa-
+    # type na classe mãe -Pessoa-
     __mapper_args__ = {
         "polymorphic_identity": "professor",
     }
 
-    #expressao da classe no formato json
+    # expressao da classe no formato json
     def json(self):
         return {
             "id": self.id,
             "salario": self.salario,
+            "nome": self.nome,
+            "email": self.email,
+            "senha": self.senha,
+            "turma_id": self.turma_id,
+            "type": self.type
         }
