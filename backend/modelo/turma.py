33c038597183eb8e46476b7d54df6e1e7f9ea8c9
Júlia Luz <94867831+jutluz@users.noreply.github.com>
2022-12-12 23:09:47 -0300

    Atualizando atributos e a função json

--- backend/modelo/turma.py
+++ backend/modelo/turma.py
@@ -2,31 +2,28 @@ from geral.config import *
 from modelo.agenda import Agenda
 
 class Turma(db.Model):
-    #criação da tabela
+    # criação da tabela
     __tablename__ = "turma"
     
-    #colunas com os atributos pra tabela no SQLAlchemy 
+    # colunas com os atributos pra tabela no SQLAlchemy 
     id = db.Column(db.Integer, primary_key=True)
-    ano = db.Column(db.Integer)
-    semestre = db.Column(db.Integer)
+    ano = db.Column(db.Integer, nullable = False)
+    semestre = db.Column(db.Integer, nullable = False)
     
-    #lista reversa de instrumentos que uma turma estuda
+    # lista reversa de instrumentos que uma turma estuda
     instrumentos = db.relationship("Instrumento", backref="turma")
 
-    #associação com a classe Pessoa
+    # associação com a classe Pessoa
     pessoa = db.relationship("Pessoa", back_populates="turma")
 
-    #agenda não pode ser nula por conta da composição
+    # autor que fez a musica não pode ser nulo por conta da composição
     agenda_id = db.Column(db.Integer, db.ForeignKey(Agenda.id), nullable = False)
     agenda = db.relationship("Agenda")
 
-    #função pra transformar os atributos em json
+    # função pra transformar os atributos em json
     def json(self):
         return {
             "id":self.id,
             "ano":self.ano,
             "semestre":self.semestre,
-            "instrumentos":self.instrumentos,
-            "agenda_id":self.agenda_id,
-            "agenda":self.agenda.json()
         }
