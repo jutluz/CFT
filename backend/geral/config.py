d569ed3b7bbc1a786310ad13fcdc45b23cd45190
Júlia Luz <94867831+jutluz@users.noreply.github.com>
2022-12-12 22:59:58 -0300

    Adicionando as importações do JWT

--- backend/geral/config.py
+++ backend/geral/config.py
@@ -1,30 +1,41 @@
-#IMPORTAÇÕES
+# IMPORTAÇÕES
 import os
 from flask import Flask, jsonify, request, session, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from flask_cors import CORS # permitir back receber dados do front
 
-#CONFIGURAÇÕES
-#cria a aplicação Flask
+# CONFIGURAÇÕES
+# cria a aplicação Flask
 app = Flask(__name__)
 
-#adiciona o Cors ao app (Flask)
+# adiciona o Cors ao app (Flask)
 CORS(app)
 
-#pega a path (caminho) deste arquivo
+# pega a path (caminho) deste arquivo
 path = os.path.dirname(os.path.abspath(__file__))
 
-#cria o arquivo do banco de dados
+# cria o arquivo do banco de dados
 arquivobd = os.path.join(path, 'data.db')
 
-#configurações do sqlalchemy ()
+# configurações do sqlalchemy ()
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"+arquivobd
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-#configurações da sessão
-app.secret_key = 'kW0Ws9q9lg#jdE4WP7FmTTFBVk7EdY' #chave secreta
+# configurações da sessão
+app.secret_key = 'kW0Ws9q9lg#jdE4WP7FmTTFBVk7EdY' # chave secreta
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app) #adiciona a sessão ao app
+Session(app) # adiciona a sessão ao app
+
+# importações de JWT
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
+
+app.config["JWT_SECRET_KEY"] = "super-secret"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=10)
+jwt = JWTManager(app)
