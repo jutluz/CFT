231b812b00b8cbca1d6bf23f0efdde4ed1a25766
Júlia Luz <94867831+jutluz@users.noreply.github.com>
2022-12-12 23:04:25 -0300

    Ajeitando comentários

--- backend/modelo/agenda.py
+++ backend/modelo/agenda.py
@@ -1,18 +1,18 @@
 from geral.config import *
 
 class Agenda(db.Model):
-    #criação da tabela
+    # criação da tabela
     __tablename__ = "agenda"
 
-    #atributos da agenda
+    # atributos da agenda
     id = db.Column(db.Integer, primary_key=True)
-    dia_semana = db.Column(db.String(254), nullable = False)
-    horario = db.Column(db.Float, nullable = False)
+    dia_semana = db.Column(db.String(100), nullable = False)
+    # horario = db.Column(db.Time, nullable = False)
 
-    #expressao da classe no formato json
+    # expressao da classe no formato json
     def json(self):
         return {
             "id": self.id,
             "dia_semana": self.dia_semana,
-            "horario": self.horario
+            # "horario": self.horario
         }
